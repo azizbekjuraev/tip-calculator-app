d5bd03c9b9f6b9398044fd54890431ce4e5c462e
AZIZBEKJURAEV <ajuraev98@mail.ru>
2023-03-10 07:32:45 +0500

    custom tip added

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const tipButtons = document.querySelectorAll(".primary");
 const totalTip = document.getElementById("total-tip");
 const tipAmount = document.getElementById("tip-amount");
 const resetButton = document.querySelector(".reset-btn");
+const customTipButton = document.querySelector(".custom");
 
-console.log(tipButtons);
-
+//Selecting tips
 tipButtons.forEach((b) =>
   b.addEventListener("click", function () {
-    console.log(b);
-    b.classList.add("active");
     let convertToNum = Number(b.id);
     let tipValue = (convertToNum / 100) * bills.value;
     totalTip.textContent = `$${tipValue}`;
@@ -20,10 +18,20 @@ tipButtons.forEach((b) =>
   })
 );
 
+//Reset to default
 resetButton.addEventListener("click", function () {
-  console.log(`reset was pressed`);
   totalTip.textContent = `$0.00`;
   tipAmount.textContent = `$0.00`;
   bills.value = "";
   numberOfPeople.value = "";
+});
+
+//Custom tip
+customTipButton.addEventListener("click", () => {
+  let tipValueOfPrompt = prompt(`Write your % here! like(25, 30, 40)`);
+  let tipValueOfPromptToNumber = Number(tipValueOfPrompt);
+  let customTipValue = (tipValueOfPromptToNumber / 100) * bills.value;
+
+  totalTip.textContent = `$${customTipValue}`;
+  tipAmount.textContent = `$${customTipValue / numberOfPeople.value}`;
 });
