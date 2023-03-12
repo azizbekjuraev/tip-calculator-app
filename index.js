cf7a95656f9cd2b2f0f1072d3b0aac1269449da5
AZIZBEKJURAEV <ajuraev98@mail.ru>
2023-03-12 10:50:41 +0500

    improved jquery use

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 `use strict`;
-const numberOfPeople = document.querySelector(".split");
-const bills = document.querySelector(".bill");
-const tipButtons = document.querySelectorAll(".primary");
-const totalTip = document.getElementById("total-tip");
-const tipAmount = document.getElementById("tip-amount");
-const resetButton = document.querySelector(".reset-btn");
 
 //Selecting tips
 $("button").each(function (i, value) {
   $(value).click((b) => {
     let convertToNum = Number(b.target.id);
-    let tipValue = (convertToNum / 100) * bills.value;
-    totalTip.textContent = `$${tipValue}`;
-    tipAmount.textContent = `$${tipValue / numberOfPeople.value}`;
-    resetButton.disabled = false;
+    let tipValue = (convertToNum / 100) * $(".bill").val();
+    $("#total-tip").text(`$${tipValue}`);
+    $("#tip-amount").text(`$${tipValue / $(".split").val()}`);
+    $(".reset-btn").removeAttr("disabled");
   });
 });
 
 //Resetting
 $(".reset-btn").click(function () {
-  totalTip.textContent = `$0.00`;
-  tipAmount.textContent = `$0.00`;
-  bills.value = "";
-  numberOfPeople.value = "";
+  $("#total-tip").text(`$0.00`);
+  $("#tip-amount").text(`$0.00`);
+  $(".bill").val("");
+  $(".split").val("");
 });
 
 //Custom tip
 $(".custom").click(function () {
   let tipValueOfPrompt = prompt(`Write your % here! like(25, 30, 40)`);
   let tipValueOfPromptToNumber = Number(tipValueOfPrompt);
-  let customTipValue = (tipValueOfPromptToNumber / 100) * bills.value;
-  totalTip.textContent = `$${customTipValue}`;
-  tipAmount.textContent = `$${customTipValue / numberOfPeople.value}`;
+  let customTipValue = (tipValueOfPromptToNumber / 100) * $(".bill").val();
+  $("#total-tip").text(`$${customTipValue}`);
+  $("#tip-amount").text(`$${customTipValue / $(".split").val()}`);
 });
